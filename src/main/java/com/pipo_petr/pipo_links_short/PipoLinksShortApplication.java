83954d2ba29c2c5a9cd18602deb1c8ea0fa54b4d
FertyTry <merzlikin.0404@mail.ru>
2026-02-18 15:17:05 +0300

    init commit

---
+++ src/main/java/com/pipo_petr/pipo_links_short/PipoLinksShortApplication.java
@@ -0,0 +1,13 @@
+package com.pipo_petr.pipo_links_short;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipoLinksShortApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PipoLinksShortApplication.class, args);
+    }
+
+}
